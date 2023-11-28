9b321eccfa65f90893b8382780c2de1889503ebd
Harsha-ah <harshasukumarand503@gmail.com>
2023-11-29 00:38:33 +0530

    swapping first letter of strings

---
+++ colorlist.py
@@ -0,0 +1,3 @@
+list1=set(['blue','black','yellow','white'])
+list2=set(['white','black','skyblue','pink','purple'])
+list1.difference(list2)
---
+++ swap.py
@@ -0,0 +1,3 @@
+a=input("enter first string:")
+b=input("enter second string:")
+print(b[0]+a[1:],"",a[0]+b[1:])
