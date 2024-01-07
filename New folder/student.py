9159e0efad84a49760ae18a32a188f836bb1473b
Harsha-ah <harshasukumarand503@gmail.com>
2024-01-07 11:49:05 +0530

    print given

---
+++ New folder/student.py
@@ -0,0 +1,8 @@
+class Student:
+    def __init__(self,rlno,name):
+        self.rlno=rlno
+        self.name=name
+
+stud1=Student(21,"harsha")
+print("roll no :",stud1.rlno)
+print("name :",stud1.name)
